5294e81204a51b66372a2ba256fc36746cf5bbd7
Burnie <W0410404@campus.nscc.ca>
2023-09-13 15:07:33 -0300

    Basic initialization

--- HudVariables/HudVariables/Program.cs
+++ HudVariables/HudVariables/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HudVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* INT initializations */ int score = 0; int health = 0;  int damage = 0; int enenmyHealth = 0;
+            /* String initializations */ string studioName = "NameWasTakenStudios";
+            /* Float initializations */float scoreMult = 1.00f; float lives = 3.00f;
+
+            Console.WriteLine("Score: " + score + " Health: " + health + " Lives: " + lives);
+
+            // score = score + enemyValue ||  score+= enemyValue;
+            
+        }
+    }
+}
