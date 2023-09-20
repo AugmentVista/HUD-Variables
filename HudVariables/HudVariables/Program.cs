fc98c23bc5f42d914b855f593ee792c511d5fab8
Burnie <W0410404@campus.nscc.ca>
2023-09-20 12:53:39 -0300

    Kept old difficulty system add rnd
    random 1-20 for player and 1-10 for enemy attacks that accept modifers

--- HudVariables/HudVariables/Program.cs
+++ HudVariables/HudVariables/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,22 +13,25 @@ namespace HudVariables
         class Questions
         {
             /* Int declarations */
-            public int score; public int health; public int damage; public int enenmyHealth; public static int selectedDifficulty;
+            public int score; public int health; public int playerDamage; public int enenmyHealth; 
+            public static int selectedDifficulty; public static int enemyDamage; public static int level = 1;
+            public static int activeWeapon; public static int Stick = 2; 
             /* String declarations */
-            public string question; public string correctAnswer; public string userAnswer; public string studioName; public string finalAnswer;
-            public string playerName;
+            public string question; public string correctAnswer; public string userAnswer; public string studioName; 
+            public string finalAnswer; public string playerName;
             /* Float declarations */
-            float scoreMult; float lives;
+           static float scoreMult; static float lives;
+            /* Bool declarations */
+            public static ConsoleKeyInfo input;
+            private static Random randomResult;
 
-
-            /* Requests a difficulty answer from the user and only accepts easy, medium or hard
-               if it doesn't get one of these it resets */
             public Questions(string aQuestion, string aCorrectAnswer)
             {
                 question = aQuestion;
                 correctAnswer = aCorrectAnswer;
             }
 
+            // Checks if the answer is either easy medium or hard
             public bool ValidateAnswer(string aUserAnswer)
             {
                 if (aUserAnswer == "easy" || aUserAnswer == "medium" || aUserAnswer == "hard")
@@ -37,6 +42,8 @@ namespace HudVariables
                 Console.WriteLine("Please choose between \"easy\", \"medium\" or \"hard\".");
                 return false;
             }
+
+            //checks that the answer given by the user is acceptable
             public bool VerifyAnswer(string aUserAnswer)
             {
                 return aUserAnswer == correctAnswer;
@@ -45,7 +52,7 @@ namespace HudVariables
             static void Main(string[] args)
             {
                 /* Int initializations */
-                int score = 0; int health = 0; int damage = 0; int enenmyHealth = 0;
+                int score = 0; int health = 0; int playerDamage = 0; int enenmyHealth = 0;
                 /* String initializations*/
                 string studioName = "NameWasTakenStudios"; string playerName;
                 /* Float initializations */
@@ -89,30 +96,84 @@ namespace HudVariables
                 {
                     scoreMult = 1.00f;
                     lives = 5;
+                    playerDamage = 5;
                 }
                 else if (answer == "medium")
                 {
                     scoreMult = 1.5f;
                     lives = 4;
+                    playerDamage = 7;
                 }
                 else if (answer == "hard")
                 {
                     scoreMult = 2.0f;
                     lives = 3;
+                    playerDamage = 9;
                 }
+                Console.Clear();
                 ShowHud();
+                Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine("                                                         LEVEL 1");
+                Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine("You begin your journey with a sturdy stick and a smile on your face.");
+                Console.WriteLine();
+                int activeWeapon = Stick;
+                Console.WriteLine("before you stands your first enemy, a goblin that stole your lunch");
+                Console.WriteLine("Press 'E' to Attack");
+                KeyInput();
                 Console.ReadKey();
 
 
 
 
 
+                // Various Methods
+                void KeyInput()
+                {
+                    input = Console.ReadKey(true);
 
 
-                void ShowHud()
+                    switch (input.Key)
+                    {
+
+                        case ConsoleKey.E:
+                            {
+                                Console.WriteLine("check check"); 
+                            Attack();
+
+                            }
+                            break;
+
+                        case ConsoleKey.Q:
+                            //Talk();
+
+                            break;
+
+
+                    }
+                }
+                void Attack()
+                {
+                    Console.WriteLine("is this working");
+                    Random rnd = new Random();
+
+                    
+                    int playerAttack = rnd.Next(1, 20) + playerDamage + activeWeapon;
+                    Console.WriteLine(playerAttack);
+
+                    int enemyAttack = rnd.Next(1, 10) + enemyDamage;
+                 
+                    
+
+
+
+                }
+                    void ShowHud()
                 {
                     Console.WriteLine("Score: " + score + " Health: " + health + " Lives: " + lives + " Score Multiplier " + scoreMult);
-                   
+                    Console.WriteLine(                                                        "LEVEL " + level );
 
                 }
             }
